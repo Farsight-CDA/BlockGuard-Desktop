3b8b4679c887d56dd95b9690eaa2bbc5888eeebd
Playwo <eliaswolf1609@gmail.com>
2023-09-19 16:11:21 +0200

    Implement MTLSFetch

--- app.go
+++ app.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"fmt"
+	"crypto/tls"
+	"io"
+	"net/http"
 )
 
 // App struct
@@ -21,7 +24,48 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+
+func (a *App) MTLSFetch(method string, path string, body string, csr string, privateKey string) any {
+	cert, err := tls.X509KeyPair([]byte(csr), []byte(privateKey))
+
+	if err != nil {
+		return "Fail 1"
+	}
+
+	certs := []tls.Certificate{cert}
+
+	tr := &http.Transport{
+        TLSClientConfig: &tls.Config{Certificates: certs,
+        InsecureSkipVerify: true},
+    }
+
+	client := &http.Client{Transport: tr}
+
+	if err != nil {
+		return "Fail 2"
+	}
+
+	req, err := http.NewRequest(method, path, bytes.NewReader([]byte(body)));
+
+	if err != nil {
+		return "Fail 3"
+	}
+
+	res, err := client.Do(req);
+
+	if err != nil {
+		return "Fail 4"
+	}
+
+	result, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return "Fail 5"
+	}
+
+	if (len(result) == 0) {
+		return res.Status
+	}
+
+	return string(result[:])
 }
